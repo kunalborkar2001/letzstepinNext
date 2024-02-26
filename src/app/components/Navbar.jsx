fcf6524303363ac0675132f883411d90d9d4ca45
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-26 21:25:10 +0530

    added scroll in nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -30,42 +30,42 @@ function Navbar({ className }) {
                     </div>
                 </MenuItem>
                 <MenuItem setActive={setActive} active={active} item="Categories">
-                    <div className="  text-sm grid gird-cols-1 md:grid-cols-2 gap-10 p-4">
+                    <div className="text-sm grid gird-cols-1 md:grid-cols-2 gap-10 p-4 max-h-[50vh] overflow-y-auto">
                         <ProductItem
                             title="Fitness"
                             href="/fitness"
                             src="https://plus.unsplash.com/premium_photo-1664109999537-088e7d964da2?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            description="Prepare for tech interviews like never before."
+                            description="Dive in the world of fitness"
                         />
                         <ProductItem
                             title="Banquet"
                             href="/banquet"
                             src="https://images.unsplash.com/photo-1572319663329-ac47c4efdef0?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            description="Production ready Tailwind css components for your next project"
+                            description="The perfect venue for weddings, receptions, and corporate events"
                         />
                         <ProductItem
                             title="Real-Estate"
                             href="/realestate"
                             src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fFJlYWwlMjBFc3RhdGV8ZW58MHx8MHx8fDA%3D"
-                            description="Never write from scratch again. Go from idea to blog in minutes."
+                            description="Discover your dream home with our real estate services"
                         />
                         <ProductItem
                             title="Education"
                             href="education"
                             src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZWR1Y2F0aW9ufGVufDB8fDB8fHww"
-                            description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+                            description="Transform education with cutting-edge technology and resources"
                         />
                         <ProductItem
                             title="Hotels"
                             href="/hotels"
                             src="https://images.unsplash.com/photo-1590447158019-883d8d5f8bc7?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D"
-                            description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+                            description="Stay in comfort and luxury at our top-rated hotels"
                         />
                         <ProductItem
                             title="Co-Working"
                             href="/coworking"
                             src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y28lMjB3b3JraW5nfGVufDB8fDB8fHww"
-                            description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+                            description="Discover the perfect workspace solution for your business at our co-working spaces"
                         />
                     </div>
                 </MenuItem>
